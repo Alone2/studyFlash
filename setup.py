55f6aec05d8595dc1420af5fcd8510d5d7d968e7
Alone2 <alain.sinzig@gmail.com>
2019-10-26 22:15:59 +0200

    lowercase scripts

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='studyFlash',
-    version='0.5.2',
+    version='0.5.3',
     description="Learning flashcards inside your terminal",
     author="Alone2",
     author_email="admin@bundr.net",
     url="https://github.com/Alone2/studyFlash",
     packages=['studyFlash'],
-    scripts=['scripts/studyFlash', 'scripts/studyFlash-quizlet']
+    scripts=['scripts/studyflash', 'scripts/studyflash-quizlet']
 )
