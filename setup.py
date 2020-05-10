4bc0b851d2fdeb8ccc0e23c4ae5b9ffec9f487e5
Alone2 <alain.sinzig@gmail.com>
2020-05-10 22:39:13 +0200

    version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='studyFlash',
-    version='0.6.0',
+    version='1.0.0',
     description="Learning flashcards inside your terminal",
     author="Alone2",
-    author_email="admin@bundr.net",
+    author_email="contact@asinz.ch",
     url="https://github.com/Alone2/studyFlash",
     packages=['studyFlash'],
     scripts=['scripts/studyflash', 'scripts/studyflash-quizlet']
