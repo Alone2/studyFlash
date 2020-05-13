c91f805f59196a1c719a9088fcfd46784b109e5c
Alone2 <alain.sinzig@gmail.com>
2020-05-13 23:22:18 +0200

    clean things up + man page

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from distutils.core import setup
 
 setup(
     name='studyFlash',
-    version='1.0.1',
+    data_files = [
+        ('man/man1', ['STUDYFLASH.1'])
+    ],
+    version='1.1.0',
     description="Learning flashcards inside your terminal",
     author="Alone2",
     author_email="contact@asinz.ch",
